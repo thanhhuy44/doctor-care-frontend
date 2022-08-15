c916741c5a622b4f507ee6ce5cb0692e09a39c47
Thanh Huy <thhuy444111@gmail.com>
2022-08-15 14:49:09 +0700

    update

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,24 @@
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './Button.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Button() {
+function Button(props) {
+    const { type, size, className, to, href, onClick, ...passProps } = props;
+    const handle = { onClick, ...passProps };
+    var Comp = 'button';
+    if (to) {
+        Comp = Link;
+        handle.to = to;
+    } else if (href) {
+        Comp = 'a';
+        handle.href = href;
+    }
     return (
-        <div>
-            <h1>This is Button</h1>
-        </div>
+        <Comp className={cx('button', type, size, className)} {...handle}>
+            This is Button
+        </Comp>
     );
 }
 
--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar, faCirclePlus, faLocationDot, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import styles from './Booking.module.scss';
+import Button from '~/components/Button/Button';
 
 const cx = classNames.bind(styles);
 
@@ -61,6 +62,61 @@ function Booking() {
                     </label>
                     <input className={cx('input')} id="userName" placeholder="Họ tên bệnh nhân (bắt buộc)" />
                 </div>
+                <div className={cx('payment')}>
+                    <span>
+                        <input type="radio" value="payment" name="type" checked /> Thanh toán sau tại cơ sở y tế
+                    </span>
+                </div>
+                <div className={cx('cost')}>
+                    <p className={cx('line', 'price')}>
+                        <span>Giá khám</span>
+                        <span>250.000đ</span>
+                    </p>
+                    <p className={cx('line', 'extra-fee')}>
+                        <span>Phí đặt lịch</span>
+                        <span>Miễn phí</span>
+                    </p>
+                    <p className={cx('line', 'total-cost')}>
+                        <span>Tổng cộng</span>
+                        <span>250.000đ</span>
+                    </p>
+                </div>
+                <div className={cx('warning')}>
+                    <p>Quý khách vui lòng điền đầy đủ thông tin để tiết kiệm thời gian làm thủ tục khám</p>
+                </div>
+                <div className={cx('notice')}>
+                    <h3 className={cx('title-notice')}>LƯU Ý</h3>
+                    <div className={cx('content-notice')}>
+                        <p className={cx('rule')}>
+                            1. Thông tin bạn cung cấp sẽ được sử dụng làm hồ sơ khám bệnh. Vì vậy khi điền thông tin,
+                            bạn vui lòng lưu ý:
+                            <ul>
+                                <li>Ghi rõ họ và tên, viết hoa những chữ cái đầu tiên, ví dụ: Trần Văn Phú</li>
+                                <li>Điền đầy đủ, đúng và kiểm tra lại thông tin trước khi ấn "Xác nhận đặt khám"</li>
+                            </ul>
+                        </p>
+                        <p className={cx('rule')}>
+                            2. Tuân thủ quy định phòng chống dịch (đeo khẩu trang, khử khuẩn, khai báo dịch tễ) khi đến
+                            khám.
+                        </p>
+                    </div>
+                </div>
+                <div className={cx('submit')}>
+                    <Button type="primary" className={cx('submit-btn')}>
+                        Xác nhận đặt khám
+                    </Button>
+                </div>
+                <p className={cx('confirm')}>
+                    Bằng việc xác nhận đặt khám, bạn đã hoàn toàn đồng ý với{' '}
+                    <a
+                        className={cx('confirm-link')}
+                        href="https://bookingcare.vn/thong-tin/dieu-khoan-su-dung-p7"
+                        target="blank"
+                    >
+                        Điều khoản sử dụng dịch vụ
+                    </a>{' '}
+                    của chúng tôi.
+                </p>
             </div>
         </div>
     );
