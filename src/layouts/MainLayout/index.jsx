ae0e40f0f0e650e309ff6694840cf145714728af
thanhhuy44 <thhuy444111@gmail.com>
2022-11-07 11:43:43 +0700

    update

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,10 +1,5 @@
-import classNames from 'classnames/bind';
-import styles from './MainLayout.module.scss';
-
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
-const cx = classNames.bind(styles);
 
 function MainLayout({ children }) {
     return (
@@ -13,7 +8,7 @@ function MainLayout({ children }) {
                 <Header />
             </div>
             <div className="mt-[80px] min-h-[70vh] relative">{children}</div>
-            <div className={cx('footer')}>
+            <div>
                 <Footer />
             </div>
         </div>
